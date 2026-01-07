f0e6827a2a57902c71402592c68077b8d0cfa19b
RVelasquezAlbites <120272668+RicardoB3@users.noreply.github.com>
2026-01-07 09:46:14 -0500

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,188 +1,125 @@
 #!/bin/bash
-cd $HOME
-SCPdir="/etc/newadm"
-SCPinstal="$HOME/install"
-SCPidioma="${SCPdir}/idioma"
-SCPusr="${SCPdir}/ger-user"
-SCPfrm="/etc/ger-frm"
-SCPinst="/etc/ger-inst"
-[[ $(dpkg --get-selections|grep -w "gawk"|head -1) ]] || apt-get install gawk -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "mlocate"|head -1) ]] || apt-get install mlocate -y &>/dev/null
-rm $(pwd)/$0 &> /dev/null
+# Script Instalador Adaptado para RicardoB3/Ultimate-VPS
+# Soporte para carpetas 'gerador' e 'Install'
+# Compatible con Ubuntu 20.04 y 22.04
+
+# --- TUS DATOS DE GITHUB ---
+USUARIO="RicardoB3"
+REPO="Ultimate-VPS"
+RAMA="master"
+# ---------------------------
+
+URL_RAW="https://raw.githubusercontent.com/${USUARIO}/${REPO}/${RAMA}"
+DIR_BASE="/etc/newadm"
+DIR_USER="${DIR_BASE}/ger-user"
+DIR_INST="/etc/ger-inst"
+DIR_HERR="/etc/ger-frm"
+
+# Colores
 msg () {
-BRAN='\033[1;37m' && VERMELHO='\e[31m' && VERDE='\e[32m' && AMARELO='\e[33m'
-AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' &&NEGRITO='\e[1m' && SEMCOR='\e[0m'
- case $1 in
-  -ne)cor="${VERMELHO}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}";;
-  -ama)cor="${AMARELO}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-  -verm)cor="${AMARELO}${NEGRITO}[!] ${VERMELHO}" && echo -e "${cor}${2}${SEMCOR}";;
-  -azu)cor="${MAG}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-  -verd)cor="${VERDE}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-  -bra)cor="${BRAN}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}";;
-  -bar2)cor="${AZUL}${NEGRITO}======================================================" && echo -e "${cor}${SEMCOR}";;
-  -bar)cor="${AZUL}${NEGRITO}========================================" && echo -e "${cor}${SEMCOR}";;
- esac
+    BRAN='\033[1;37m' && VERMELHO='\e[31m' && VERDE='\e[32m' && AMARELO='\e[33m'
+    AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' && NEGRITO='\e[1m' && SEMCOR='\e[0m'
+    case $1 in
+        -ne) echo -ne "${VERMELHO}${NEGRITO}${2}${SEMCOR}";;
+        -ama) echo -e "${AMARELO}${NEGRITO}${2}${SEMCOR}";;
+        -verm) echo -e "${AMARELO}${NEGRITO}[!] ${VERMELHO}${2}${SEMCOR}";;
+        -azu) echo -e "${MAG}${NEGRITO}${2}${SEMCOR}";;
+        -verd) echo -e "${VERDE}${NEGRITO}${2}${SEMCOR}";;
+        -bar) echo -e "${AZUL}${NEGRITO}====================================================${SEMCOR}";;
+    esac
 }
-fun_ip () {
-MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-MIP2=$(wget -qO- ipv4.icanhazip.com)
-[[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
-}
-inst_components () {
-[[ $(dpkg --get-selections|grep -w "nano"|head -1) ]] || apt-get install nano -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] || apt-get install screen -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "python"|head -1) ]] || apt-get install python -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "python3"|head -1) ]] || apt-get install python3 -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || apt-get install curl -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "ufw"|head -1) ]] || apt-get install ufw -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "unzip"|head -1) ]] || apt-get install unzip -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "zip"|head -1) ]] || apt-get install zip -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] || {
- apt-get install apache2 -y &>/dev/null
- sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
- service apache2 restart > /dev/null 2>&1 &
- }
-}
-funcao_idioma () {
-msg -bar2
-declare -A idioma=( [1]="en English" [2]="fr Franch" [3]="de German" [4]="it Italian" [5]="pl Polish" [6]="pt Portuguese" [7]="es Spanish" [8]="tr Turkish" )
-for ((i=1; i<=12; i++)); do
-valor1="$(echo ${idioma[$i]}|cut -d' ' -f2)"
-[[ -z $valor1 ]] && break
-valor1="\033[1;32m[$i] > \033[1;33m$valor1"
-    while [[ ${#valor1} -lt 37 ]]; do
-       valor1=$valor1" "
+
+# 1. Verificación de Root
+[[ $EUID -ne 0 ]] && msg -verm "Error: Ejecuta como root" && exit 1
+
+# 2. Instalación de Dependencias (Ubuntu 20/22 Fix)
+preparar_vps () {
+    clear
+    msg -bar
+    msg -ama "ACTUALIZANDO SISTEMA Y DEPENDENCIAS..."
+    
+    # Liberar apt si está bloqueado
+    rm -rf /var/lib/dpkg/lock-frontend /var/lib/dpkg/lock
+    
+    apt-get update -y > /dev/null 2>&1
+    
+    # Paquetes esenciales + Fix de Python para scripts antiguos
+    PAQUETES="jq bc screen curl ufw unzip zip net-tools nano python3 python3-pip python-is-python3 cron apache2 at"
+    
+    for paq in $PAQUETES; do
+        [[ $(dpkg --get-selections|grep -w "$paq"|head -1) ]] || apt-get install $paq -y &>/dev/null
+        echo -ne "\033[1;32m [OK] $paq \033[0m"
     done
-echo -ne "$valor1"
-let i++
-valor2="$(echo ${idioma[$i]}|cut -d' ' -f2)"
-[[ -z $valor2 ]] && {
-   echo -e " "
-   break
-   }
-valor2="\033[1;32m[$i] > \033[1;33m$valor2"
-     while [[ ${#valor2} -lt 37 ]]; do
-        valor2=$valor2" "
-     done
-echo -ne "$valor2"
-let i++
-valor3="$(echo ${idioma[$i]}|cut -d' ' -f2)"
-[[ -z $valor3 ]] && {
-   echo -e " "
-   break
-   }
-valor3="\033[1;32m[$i] > \033[1;33m$valor3"
-     while [[ ${#valor3} -lt 37 ]]; do
-        valor3=$valor3" "
-     done
-echo -e "$valor3"
-done
-msg -bar2
-unset selection
-while [[ ${selection} != @([1-8]) ]]; do
-echo -ne "\033[1;37mSELECT: " && read selection
-tput cuu1 && tput dl1
-done
-echo ${idioma[$selection]}|cut -d' ' -f1
-pv="$(echo ${idioma[$selection]}|cut -d' ' -f1)"
-[[ ${#id} -gt 2 ]] && id="pt" || id="$pv"
-byinst="true"
-}
-install_fim () {
-msg -ama "$(source trans -b pt:${id} "Instalacao Completa, Utilize os Comandos"|sed -e 's/[^a-z -]//ig')" && msg bar2
-echo -e " menu / adm" && msg -verm "$(source trans -b pt:${id} "Reinicie seu servidor para concluir a instalacao"|sed -e 's/[^a-z -]//ig')"
-msg -bar2
-}
-ofus () {
-unset txtofus
-number=$(expr length $1)
-for((i=1; i<$number+1; i++)); do
-txt[$i]=$(echo "$1" | cut -b $i)
-case ${txt[$i]} in
-".")txt[$i]="+";;
-"+")txt[$i]=".";;
-"1")txt[$i]="@";;
-"@")txt[$i]="1";;
-"2")txt[$i]="?";;
-"?")txt[$i]="2";;
-"3")txt[$i]="%";;
-"%")txt[$i]="3";;
-"/")txt[$i]="K";;
-"K")txt[$i]="/";;
-esac
-txtofus+="${txt[$i]}"
-done
-echo "$txtofus" | rev
-}
-verificar_arq () {
-[[ ! -d ${SCPdir} ]] && mkdir ${SCPdir}
-[[ ! -d ${SCPusr} ]] && mkdir ${SCPusr}
-[[ ! -d ${SCPfrm} ]] && mkdir ${SCPfrm}
-[[ ! -d ${SCPinst} ]] && mkdir ${SCPinst}
-case $1 in
-"menu"|"message.txt")ARQ="${SCPdir}/";; #Menu
-"usercodes")ARQ="${SCPusr}/";; #User
-"openssh.sh")ARQ="${SCPinst}/";; #Instalacao
-"squid.sh")ARQ="${SCPinst}/";; #Instalacao
-"dropbear.sh")ARQ="${SCPinst}/";; #Instalacao
-"openvpn.sh")ARQ="${SCPinst}/";; #Instalacao
-"ssl.sh")ARQ="${SCPinst}/";; #Instalacao
-"shadowsocks.sh")ARQ="${SCPinst}/";; #Instalacao
-"sockspy.sh"|"PDirect.py"|"PPub.py"|"PPriv.py"|"POpen.py"|"PGet.py")ARQ="${SCPinst}/";; #Instalacao
-*)ARQ="${SCPfrm}/";; #Ferramentas
-esac
-mv -f ${SCPinstal}/$1 ${ARQ}/$1
-chmod +x ${ARQ}/$1
-}
-fun_ip
-wget -O /usr/bin/trans https://raw.githubusercontent.com/4nth0nySLT/Ultimate-VPS/master/Install/trans &> /dev/null
-msg -bar2
-msg -ama "[ NEW - ULTIMATE - SCRIPT ]"
-[[ $1 = "" ]] && funcao_idioma || {
-[[ ${#1} -gt 2 ]] && funcao_idioma || id="$1"
- }
+    echo ""
 
-invalid_key () {
-exit 1
+    # Mover Apache al puerto 81
+    if [[ -f /etc/apache2/ports.conf ]]; then
+        sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
+        service apache2 restart > /dev/null 2>&1
+    fi
 }
 
-msg -ne "Key: "
-cd $HOME
-wget -O $HOME/lista-arq https://raw.githubusercontent.com/4nth0nySLT/Ultimate-VPS/master/gerador/GERADOR > /dev/null 2>&1 && echo -e "\033[1;32m Verified" || {
-   echo -e "\033[1;32m Verified"
-   exit
-   }
-sleep 1s
-updatedb
+# 3. Estructura de Directorios
+crear_directorios () {
+    mkdir -p ${DIR_BASE}
+    mkdir -p ${DIR_USER}
+    mkdir -p ${DIR_INST}
+    mkdir -p ${DIR_HERR}
+}
 
-if [[ -e $HOME/lista-arq ]]; then
-   msg -bar2
-   msg -ama "$(source trans -b pt:${id} "BEM VINDO, OBRIGADO POR UTILIZAR"|sed -e 's/[^a-z -]//ig'): \033[1;31m[NEW-ULTIMATE]"
-   [[ ! -d ${SCPinstal} ]] && mkdir ${SCPinstal}
-   pontos="."
-   stopping="$(source trans -b pt:${id} "Verificando Atualizacoes"|sed -e 's/[^a-z -]//ig')"
-   for arqx in $(cat $HOME/lista-arq); do
-   msg -verm "${stopping}${pontos}"
-   echo https://raw.githubusercontent.com/4nth0nySLT/Ultimate-VPS/master/gerador/${arqx} > /dev/null 2>&1
-   wget -O ${SCPinstal}/${arqx} https://raw.githubusercontent.com/4nth0nySLT/Ultimate-VPS/master/gerador/${arqx} > /dev/null 2>&1 && verificar_arq "${arqx}"
-   tput cuu1 && tput dl1
-   pontos+="."
-   done
-   sleep 1s
-   msg -bar2
-   listaarqs="$(locate "lista-arq"|head -1)" && [[ -e ${listaarqs} ]] && rm $listaarqs   
-   cat /etc/bash.bashrc|grep -v '[[ $UID != 0 ]] && TMOUT=15 && export TMOUT' > /etc/bash.bashrc.2
-   echo -e '[[ $UID != 0 ]] && TMOUT=15 && export TMOUT' >> /etc/bash.bashrc.2
-   mv -f /etc/bash.bashrc.2 /etc/bash.bashrc
-   echo "${SCPdir}/menu" > /usr/bin/menu && chmod +x /usr/bin/menu
-   echo "${SCPdir}/menu" > /usr/bin/adm && chmod +x /usr/bin/adm
-   Key=4nth0nySLT
-   echo "$Key" > ${SCPdir}/key.txt
-   inst_components
-   [[ -d ${SCPinstal} ]] && rm -rf ${SCPinstal}   
-   [[ ${#id} -gt 2 ]] && echo "pt" > ${SCPidioma} || echo "${id}" > ${SCPidioma}
-   [[ ${byinst} = "true" ]] && install_fim
-else
-invalid_key
-fi
+# 4. Descarga Inteligente (Soporte para carpetas)
+descargar_archivos () {
+    msg -bar
+    msg -ama "DESCARGANDO ARCHIVOS DESDE GITHUB..."
+    
+    # A) Descargar archivo de traducción desde carpeta 'Install'
+    msg -azu "Descargando traducciones..."
+    wget -O /usr/bin/trans ${URL_RAW}/Install/trans &>/dev/null
+    chmod +x /usr/bin/trans
+
+    # B) Descargar la lista 'GERADOR' desde carpeta 'gerador'
+    msg -azu "Obteniendo lista de scripts..."
+    wget -O ${DIR_BASE}/lista-arq ${URL_RAW}/gerador/GERADOR &>/dev/null
+    
+    if [[ ! -s ${DIR_BASE}/lista-arq ]]; then
+        msg -verm "ERROR: No se encontró el archivo 'gerador/GERADOR' en tu GitHub."
+        exit 1
+    fi
+
+    # C) Bucle de descarga desde carpeta 'gerador'
+    for arqx in $(cat ${DIR_BASE}/lista-arq); do
+        msg -azu "Descargando: $arqx"
+        
+        # Descarga desde la carpeta /gerador/
+        wget -O ${DIR_BASE}/${arqx} ${URL_RAW}/gerador/${arqx} &>/dev/null
+        chmod +x ${DIR_BASE}/${arqx}
+        
+        # Distribución de archivos
+        if [[ "$arqx" == "menu" ]]; then
+            mv ${DIR_BASE}/${arqx} /usr/bin/menu
+            chmod +x /usr/bin/menu
+            chmod +x /usr/bin/adm
+        elif [[ "$arqx" == "usercodes" ]]; then
+             mv ${DIR_BASE}/${arqx} ${DIR_USER}/
+        elif [[ "$arqx" == *.sh ]]; then
+             mv ${DIR_BASE}/${arqx} ${DIR_INST}/
+        else
+             mv ${DIR_BASE}/${arqx} ${DIR_HERR}/
+        fi
+    done
+}
+
+# 5. Finalización
+finalizar () {
+    msg -bar
+    msg -verd " INSTALACIÓN COMPLETADA "
+    msg -ama " Escribe 'menu' para entrar"
+    msg -bar
+    rm -rf $0
+}
+
+# Ejecución
+preparar_vps
+crear_directorios
+descargar_archivos
+finalizar
